1ad9f9142fd8c1d97172bd5689ebd08a2042f776
sam <swzwij@gmail.com>
2021-09-16 17:58:57 +0200

    commit

---
+++ Assets/scripts/BarrelRotation.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelRotation : MonoBehaviour
+{
+    public float speed = 30f;
+
+    void Update()
+    {
+        transform.localRotation *= Quaternion.AngleAxis(speed * Time.deltaTime, Vector3.forward);
+    }
+}
