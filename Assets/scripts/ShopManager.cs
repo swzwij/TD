d0ce71084cfc64e86a2e57123897e2b1ae56c341
Samuel <70766878+swzwij@users.noreply.github.com>
2021-09-25 15:13:00 +0200

    improved shop ui

--- Assets/scripts/ShopManager.cs
+++ Assets/scripts/ShopManager.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class ShopManager : MonoBehaviour
 {
     public GameObject Shop;
+    public Camera Cam1;
+    public Camera Cam2;
+
+    private void Start()
+    {
+        Cam1.GetComponent<Camera>().enabled = true;
+        Cam2.GetComponent<Camera>().enabled = false;
+    }
 
     public void OpenShop()
     {
+        print("Opened shop");
+
         Shop.SetActive(true);
+
+        Cam1.GetComponent<Camera>().enabled = false;
+        Cam2.GetComponent<Camera>().enabled = true;
     }
 
     public void CloseShop()
     {
+        print("Closed shop");
+
         Shop.SetActive(false);
+
+        Cam2.GetComponent<Camera>().enabled = false;
+        Cam1.GetComponent<Camera>().enabled = true;
     }
 }
